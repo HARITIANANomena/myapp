f2e6cb6d8d01c1fe641428ad0108b79f13f914ec
Haritiana <haritiananomenjanahary3@gmail.com>
2024-01-25 14:17:06 +0300

    Ajout dossier : component et pages avec les extension

---
+++ src/pages/SingleEvent.jsx
@@ -0,0 +1,16 @@
+
+export default function SingleEvent() {
+  return (
+    <>
+      <div className="ctn_single_evenement h-screen bg-yellow-300">
+        <div className="ctn_presentation_evenement">
+          <div>Pictur</div>
+          <div>Information d'evenemnt</div>
+        </div>
+        <div>
+          Table principale
+        </div>
+      </div>
+    </>
+  )
+}
