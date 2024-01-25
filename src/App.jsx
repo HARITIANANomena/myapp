a5e6db9250a2d73dc15efeeaf0cb0ad1ae6af7c2
Haritiana <haritiananomenjanahary3@gmail.com>
2024-01-25 14:48:01 +0300

    Changement de App.jsx a jour

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Navigation from './pages/Navigation'
+import PageNavigation from './pages/PageEvenement'
+import CreationEvenement from './pages/CreationEvenement'
+import SingleEvent from './pages/SingleEvent'
+
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <div>
-
-        <h1>Bienvenu Sur mon super site</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, est.</p>
-      </div>
-      
+      <Navigation/>
+      <Routes>     
+        <Route path='/' element={ <PageNavigation/> }/>
+        <Route path='/creation-event' element={ <CreationEvenement/> }/>
+        <Route path='/single-event' element={ <SingleEvent/> }/>
+      </Routes>
     </>
   )
 }
