957fb1466d178fc8530ad99c44f623cc2d801525
Haritiana <haritiananomenjanahary3@gmail.com>
2024-01-25 14:10:57 +0300

    Ajout d'un paragraphe

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <div>
 
         <h1>Bienvenu Sur mon super site</h1>
-        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, est.</p>
       </div>
       
     </>
