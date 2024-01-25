8f47a0b6d9499b7c55b7e9d7c4734635003a0304
Haritiana <haritiananomenjanahary3@gmail.com>
2024-01-25 13:46:50 +0300

    Modification de titre

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
-import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
 
-        <h1>Bienvenu</h1>
+        <h1>Bienvenu Sur mon super site</h1>
         
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
